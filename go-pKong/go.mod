d3f2dc75a28b16531f6b3989960391314993af37
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-02 11:26:13 +0700

    firstcommit

---
+++ go-pKong/go.mod
@@ -0,0 +1,3 @@
+module go-pKong
+
+go 1.18
---
+++ go-pKong/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello P'Kong")
+	fmt.Println("How are you?")
+}
+
+// golang is static type . Variable must has value.
+// has 2 types
+// - manual type declaration
+// - type inference
+
+// ---- Manual type declaration ----
+
+// var <name> <type>
+
+// func main() {
+// 	fmt.Println()
+// }

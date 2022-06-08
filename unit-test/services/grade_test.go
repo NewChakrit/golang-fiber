35d665787d5f10354dfe0387a6d7760c9feeda46
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-08 09:21:24 +0700

    first commit

---
+++ unit-test/services/grade_test.go
@@ -0,0 +1,12 @@
+package services_test
+
+import "testing"
+
+func TestCheckGrade(t *testing.T) {
+
+}
+
+//  ----- terminal ------
+//	1. cd to folder test
+//  2. cli "go test"
+//  3. check test "go test -v"

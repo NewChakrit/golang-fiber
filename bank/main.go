07df43275b4e825d476da61d0c03283364d1802f
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-09 16:21:43 +0700

    get all customer query

--- bank/main.go
+++ bank/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bank/repository"
+	"fmt"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	db, err := sqlx.Open("mysql", "root:P@ssw0rd@tcp(13.73.163.73:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+
+	customerRepository := repository.NewCustomerRepositoryDB(db)
+
+	_ = customerRepository
+
+	customers, err := customerRepository.GetAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customers)
 }

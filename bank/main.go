17b038a43cb0fbc387626ea300390a84efb646ed
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-10 15:35:42 +0700

    viper func_initDatabase+initTimeZone+initConfig

--- bank/main.go
+++ bank/main.go
@@ -3,33 +3,40 @@ package main
 import (
 	"bank/handler"
 	"bank/repository"
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"bank/service"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	db, err := sqlx.Open("mysql", "root:P@ssw0rd@tcp(13.73.163.73:3306)/banking")
-	if err != nil {
-		panic(err)
-	}
+	initTimeZone()
+	initConfig()
+	db := initDatabase()
 
 	customerRepositoryDB := repository.NewCustomerRepositoryDB(db)
 	customerRepositoryMock := repository.NewCustomerRepositoryMock()
 
-	_ = customerRepositoryDB
-	customerService := service.NewCustomerService(customerRepositoryMock)
+	// _ = customerRepositoryDB
+	_ = customerRepositoryMock
+
+	customerService := service.NewCustomerService(customerRepositoryDB)
 	customerHandler := handler.NewCustomerHandler(customerService)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/customers", customerHandler.Getcustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.Getcustomers).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", router)
+	log.Panicf("Banking service started at port %v", viper.GetInt("app.port"))
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
 
 	// customers, err := customerService.Getcustomers()
 	// if err != nil {
@@ -60,4 +67,49 @@ func main() {
 	// 	panic(err)
 	// }
 	// fmt.Println(customer)
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // Change config yaml
+	// ex cli = "APP_PORT=5000"
+	// ex cli = "DB_DRIVER=mongodb"
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetString("db.port"),
+		viper.GetString("db.database"),
+	)
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxIdleConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
 }

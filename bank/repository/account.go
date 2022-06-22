03224cda9429f779dde466d1ba0f329690b2361f
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-22 22:56:42 +0700

    account

--- bank/repository/account.go
+++ bank/repository/account.go
@@ -1,6 +1,16 @@
 package repository
 
-type AccountRepository interface{
-	Create()
-	GetAll()
+type Account struct{
+	AccountID int `db:"account_id"`
+	CustomerID int `db:"customer_id"`
+	OpeningDate int `db:"opening_date"`
+	AccountType int `db:"account_type"`
+	Amount int `db:"amount"`
+	Status int `db:"status"`
+
 }
+
+type AccountRepository interface{
+	Create(Account) (*Account, error)
+	GetAll(int) ([]Account, error)
+ }

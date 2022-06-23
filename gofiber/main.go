5539a734dc0c5e1dddcd0596197eab56a6ad90d4
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-23 13:23:46 +0700

    http

--- gofiber/main.go
+++ gofiber/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
 func main () {
+	// app:=mux.NewRouter()
 	
+	// app.HandleFunc("/hello/{id}", Hello).Methods(http.MethodGet)
+	
+	// http.ListenAndServe(":8000", app)
+
+	app:=fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world")
+	})
+
+	app.Listen(":8000")
 }
+
+// func Hello  (w http.ResponseWriter, r *http.Request) {
+// 	id := mux.Vars(r)["id"]
+// 	fmt.Println(id)
+// }
+

2a5305efcf417ae41941123f4d669d533c7199fa
chakrit tnasinpaibool <imnew108@gmail.com>
2022-06-23 13:42:06 +0700

    paramsint

--- gofiber/main.go
+++ gofiber/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main () {
-	// app:=mux.NewRouter()
-	
-	// app.HandleFunc("/hello/{id}", Hello).Methods(http.MethodGet)
-	
-	// http.ListenAndServe(":8000", app)
 
 	app:=fiber.New()
 
+	//GET
 	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
+		return c.SendString("GET: Hello world")
+	})
+
+	//POST
+	app.Post("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("POST: Hello world")
+	})
+
+	//Parameters
+	// app.Get("/hello/:name", func(c *fiber.Ctx) error {
+	// 	name := c.Params("name")
+	// 	return c.SendString("name: " + name)
+	// })
+
+	//Parameters Optional
+	app.Get("/hello/:name/:surname", func(c *fiber.Ctx) error {      // surname? มีหรือไม่มีก็ได้
+		name := c.Params("name")
+		surname := c.Params("surname")
+		return c.SendString("name: " + name + "surname: " + surname)
+	})
+
+	//ParamsInt
+	app.Get("/hello/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil{
+			return fiber.ErrBadRequest
+		}
+
+		return c.SendString(fmt.Sprintf("ID = %v", id))
 	})
 
 	app.Listen(":8000")
 }
 
-// func Hello  (w http.ResponseWriter, r *http.Request) {
-// 	id := mux.Vars(r)["id"]
-// 	fmt.Println(id)
-// }
+
 
